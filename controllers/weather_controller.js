9aa734ae3405f3c55147feb9a711fa5ff2c3a7ac
Ilia Abbasi <81512402+ilia-abbasi@users.noreply.github.com>
2025-08-26 09:46:22 +0330

    Used axios in weather_controller.js
    - Added requestThirdPartyAPI function in said file
    
    - Error handling was implemented in said file

--- controllers/weather_controller.js
+++ controllers/weather_controller.js
@@ -1,6 +1,8 @@
+const axios = require("axios");
 const { makeResponseObj } = require("../models/api_response.js");
+const thirdPartyAPI = "https://goweather.xyz";
 
-function getWeatherData(req, res) {
+async function getWeatherData(req, res) {
   const city = req.params.city;
 
   if (!city) {
@@ -11,15 +13,37 @@ function getWeatherData(req, res) {
     return res.status(422).send(resObj);
   }
 
-  const response = {}; // temp code
+  const url = `${thirdPartyAPI}/weather/${city}`;
+  const response = await requestThirdPartyAPI(url);
 
   if (response.message === "NOT_FOUND") {
     const resObj = makeResponseObj(false, "City was not found");
     return res.status(404).send(resObj);
   }
 
+  if (response.message === "FAILED") {
+    const resObj = makeResponseObj(
+      false,
+      `An error occurred while fetching data from the third-party API:
+${response.description}`
+    );
+    return res.status(500).send(resObj);
+  }
+
   const resObj = makeResponseObj(true, "Request was successful", response);
   return res.status(200).send(resObj);
+}
+
+async function requestThirdPartyAPI(url) {
+  try {
+    if (url === undefined || url === "") {
+      throw new Error("No URL was provided");
+    }
+    const response = await axios.get(url);
+    return response.data;
+  } catch (err) {
+    return { message: "FAILED", description: err };
+  }
 }
 
 function send422Response(req, res) {
